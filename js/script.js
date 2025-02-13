506f2e9425ef08b19cf330302125f4610ad3f35d
Mahamadou Amadou Habou Gremah <mahamadou8877@gmail.com>
2025-02-13 14:03:03 +0100

    mise à jour des différents modes: sombre,claire et jaune

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ function openSideBar(){
 function closeSideBar(){
     navbar.classList.remove('show');
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.documentElement;
+  const themeToggles = document.querySelectorAll("#theme-toggle"); // Selectionner tous les éléments avec id theme-toggle
+  let themeIndex = 0;
+
+  const themes = [
+      { "--couleur1": "#036", "--couleur2": "#ffd700", "--couleur3": "#ffcc007f" }, // Mode jaune (défaut)
+      { "--couleur1": "#edf9ffe9", "--couleur2": "#07090ad8", "--couleur3": "rgba(99, 103, 105, 0.748)" }, // Mode jour (1')
+      { "--couleur1": "#07090ad8", "--couleur2": "#edf9ffe9", "--couleur3": "rgba(99, 103, 105, 0.748)" }  // Mode nuit (1'')
+  ];
+
+  themeToggles.forEach(toggle => { // Pour chaque élément avec id "theme-toggle"
+      toggle.addEventListener("click", () => {
+          themeIndex = (themeIndex + 1) % themes.length; // Passe au mode suivant
+
+          // Applique les nouvelles couleurs
+          Object.keys(themes[themeIndex]).forEach(variable => {
+              root.style.setProperty(variable, themes[themeIndex][variable]);
+          });
+
+          console.log(`Mode actuel : ${themeIndex}`); // Vérifier dans la console
+      });
+  });
+});
+
+
+
